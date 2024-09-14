a0555e5f0a9e818d63b4945ad86f58417e1c651b
Susana Viruglio <susanaviruglio@hotmail.com>
2024-09-14 18:30:52 +0000

    added comment to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
