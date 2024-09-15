9785bb1e6beaec81c5debcaf1d7e176ecfe15c2d
Susana Viruglio <susanaviruglio@hotmail.com>
2024-09-15 11:19:01 +0000

    modified navigation bar border bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,19 @@ window.onscroll = () => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = set.getAttribute('id');
+        let id = sec.getAttribute('id');
         
         if(top >= offset && top < offset + height ) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=" + id + "]').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
 
             });
         }
     })
+    /*==================== sticky navbar ====================*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
 };
